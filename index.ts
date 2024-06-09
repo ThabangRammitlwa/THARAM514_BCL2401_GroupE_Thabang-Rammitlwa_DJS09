9e5592f6ae61e6f6b1478ff28ace5495ea492a5b
Thabang Rammitlwa <thabang451@gmail.com>
2024-06-09 20:36:33 +0200

    Array Types

--- index.ts
+++ index.ts
@@ -42,11 +42,13 @@ const you: {
     lastName: string;
     isReturning: boolean;
     age: number;
+    stayed: string[]
 }={
     firstName: 'Bobby',
     lastName: 'Brown',
     isReturning: true,
     age: 35,
+    stayed: ['florida-home', 'oman-flat','tokyo-bungalow']
 }
 
 
@@ -57,4 +59,4 @@ function populateUser(isReturning : boolean, userName: string ) {
     userNameDisplay!.innerHTML = userName
 }
 
-populateUser(you.isReturning, you.userName)
+populateUser(you.isReturning, you.firstName)
